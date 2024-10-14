0235aca228386e770cba9cf1996e6967965cb812
Balaji <balaji0017@gmail.com>
2024-10-14 20:36:42 +0100

    Add project files.

---
+++ WindowsAutomation/Program.cs
@@ -0,0 +1,57 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAutomation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Application app = Application.Launch("notepad.exe");
+
+            Window npwindow = app.GetMainWindow(new UIA3Automation());
+            Console.WriteLine(npwindow.Title);
+
+            //send the text in text editor
+            //npwindow.FindFirstByXPath("//*[@Name='Text Editor']").AsTextBox().Click();
+            //npwindow.FindFirstByXPath("//*[@Name='Text Editor']").AsTextBox().Enter("hello");
+
+            ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+
+            AutomationElement ele = Retry.Find(() => npwindow.FindFirstDescendant(cf.ByName("Text Editor")).DrawHighlight(), new RetrySettings() { IgnoreException = true, Timeout = TimeSpan.FromSeconds(60) });
+            ele.Click();
+            ele.AsTextBox().Enter("hello");
+
+
+            //npwindow.FindFirstByXPath("//MenuItem[@Name='File']").Click();
+
+            //npwindow.FindFirstByXPath("//*[@Name='Exit']").Click();
+            //ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            //AutomationElement ele = Retry.Find(() => npwindow.FindFirstDescendant(cf.ByName("Exit")).DrawHighlight(), 
+            //    new RetrySettings() { IgnoreException = true, Timeout = TimeSpan.FromSeconds(60) });
+            //ele.Click();
+
+            //click on don't save
+            //npwindow.FindFirstByXPath("//Button[@Name=\"Don't Save\"]").Click();
+
+            //  npwindow.FindFirstByXPath("//MenuItem[@Name='File']").AsMenuItem().Click();
+
+            FlaUI.Core.AutomationElements.Menu _menu = npwindow.FindFirstDescendant(cf.Menu()).AsMenu();
+
+            MenuItem _menuItem = _menu.Items["MenuBar"];
+            _menuItem.DrawHighlight();
+            _menuItem = _menuItem.Invoke();
+
+
+        }
+
+    }
+}
---
+++ WindowsAutomation/Zoom.cs
@@ -0,0 +1,52 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAutomation
+{
+    public class Zoom
+    {
+        static void Main(string[] args)
+        {
+            Application app = Application.Launch(@"C:\Users\Balaji_Dinakaran\AppData\Roaming\Zoom\bin\Zoom.exe");
+            Thread.Sleep(3000);
+
+            Window zoomWindow= app.GetMainWindow(new UIA3Automation());
+
+            //Window zoomWindow = app.GetAllTopLevelWindows(new UIA3Automation()).FirstOrDefault(w => w.Name.ToLower().Contains("Zoom workplace"));
+
+            Console.WriteLine(zoomWindow.Title);
+            Console.WriteLine(zoomWindow.Name);
+
+            //Button[@Name='Sign in']
+            zoomWindow.FindFirstByXPath("//Button[@Name='Sign in']").AsButton().Click();
+
+            //Enter your email
+            //Thread.Sleep(3000);
+            ConditionFactory cf=new ConditionFactory(new UIA3PropertyLibrary());
+            //zoomWindow.FindFirstDescendant(cf.ByName("Enter your email")).AsTextBox().Enter("jjjj");
+
+            Thread.Sleep(3000);
+            AutomationElement ele = Retry.Find(() => zoomWindow.FindFirstDescendant(cf.ByName("Enter your password")).DrawHighlight(), new RetrySettings() { IgnoreException = true, Timeout = TimeSpan.FromSeconds(60) });
+            ele.Click();
+            ele.AsTextBox().Enter("hello");
+
+            //zoomWindow.FindFirstByXPath("//*[@Name='Enter your password']").AsTextBox().Focus();
+            //oomWindow.FindFirstByXPath("//*[@Name='Enter your password']").AsTextBox().Enter("hello");
+
+            //Enter your password
+            //Sign in
+
+
+            // app.Close();
+            //will start at 9:55
+        }
+    }
+}
