3a5a78d410ff4bc6a940588236422c72fd81c8c8
Balaji <balaji0017@gmail.com>
2024-10-14 20:33:12 +0100

    Add project files.

---
+++ APIAutomation/GetPetshopAPITest.cs
@@ -0,0 +1,96 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIAutomation
+{
+    public class GetPetshopAPITest
+    {
+        //path parameter
+        [Test]
+        public void FindValidPetByIdTest()
+        {
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet/5", Method.Get);
+
+            var response=client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            Assert.True(response.Content.Contains("\"id\":5"));
+
+            dynamic json = JsonConvert.DeserializeObject(response.Content);
+            Console.WriteLine(json["id"]);
+            Console.WriteLine(json["status"]);
+
+            Console.WriteLine(json["category"]);
+            Console.WriteLine(json["category"]["id"]);
+
+            Console.WriteLine(json["tags"]);
+            Console.WriteLine(json["tags"][0]);
+            Console.WriteLine(json["tags"][0]["id"]);
+        }
+
+        [Test]
+        public void FindInvalidPetByIdTest()
+        {
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet/590", Method.Get);
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+        }
+
+
+        //query
+        [Test]
+        [TestCase("sold")]
+        public void FindValidPetStatusIdTest(string status)
+        {
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet/findByStatus?status="+status, Method.Get);
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Console.WriteLine(response.StatusCode);
+
+            dynamic json = JsonConvert.DeserializeObject(response.Content);
+
+            //Console.WriteLine(json);
+            Console.WriteLine(json.Count);
+            //Console.WriteLine(json[0]);
+            Console.WriteLine(json[0]["status"]);
+            Console.WriteLine(json[1]["status"]);
+
+            //verify all status are as expected
+            for (int i = 0; i < json.Count; i++)
+            {
+                Console.WriteLine(json[i]["status"]);
+
+                string actualStatus = json[i]["status"];
+                Assert.That(actualStatus, Is.EqualTo(status));
+            }
+
+            //verify all status is sold.
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+           
+        }
+
+    }
+}
---
+++ APIAutomation/PostPetshopAPITest.cs
@@ -0,0 +1,119 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace APIAutomation
+{
+    public class PostPetshopAPITest
+    {
+        [Test]
+        public void AddValidPetToStoreTest()
+        {
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddBody("{\r\n  \"id\": 800,\r\n  \"category\": {\r\n    \"id\": 0,\r\n    \"name\": \"string\"\r\n  },\r\n  \"name\": \"doggie800\",\r\n  \"photoUrls\": [\r\n    \"string\"\r\n  ],\r\n  \"tags\": [\r\n    {\r\n      \"id\": 0,\r\n      \"name\": \"string\"\r\n    }\r\n  ],\r\n  \"status\": \"available\"\r\n}");
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+
+        //Read json and post request
+        [Test]
+        public void AddValidPetToStoreTest2()
+        {
+            StreamReader reader = new StreamReader(@"C:\Users\Balaji_Dinakaran\source\repos\APIAutomationSln\APIAutomation\File\new_pet.json");
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet", Method.Post);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddBody(reader.ReadToEnd());
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        }
+        [Test]
+        public void ReadJsonArrayAddPetsTest()
+        {
+            StreamReader reader = new StreamReader(@"C:\Users\Balaji_Dinakaran\source\repos\APIAutomationSln\APIAutomation\File\new_pets.json");
+
+            dynamic json = JsonConvert.DeserializeObject(reader.ReadToEnd());
+
+            Console.WriteLine(json.Count);
+            for(int i = 0; i < json.Count; i++)
+            {
+                RestClient client = new RestClient("https://petstore.swagger.io/v2");
+                RestRequest request = new RestRequest("pet", Method.Post);
+                request.AddHeader("Content-Type", "application/json");
+                string pet = JsonConvert.SerializeObject(json[i]);
+                request.AddBody(pet);
+
+                var response = client.Execute(request);
+
+                Console.WriteLine(response.Content);
+                Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
+        }
+
+        //Read json and post request
+        [Test]
+        public void UpdateValidPetToStoreTest2()
+        {
+            StreamReader reader = new StreamReader(@"C:\Users\Balaji_Dinakaran\source\repos\APIAutomationSln\APIAutomation\File\update_pet.json");
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet", Method.Put);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddBody(reader.ReadToEnd());
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        }
+
+        [Test]
+        public void DeleteValidPetTest2()
+        {
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet/813", Method.Delete);
+            request.AddHeader("api_key", "jsdjsdjdsj");
+
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        }
+
+        [Test]
+        public void DeleteInvalidPetTest2()
+        {
+
+            RestClient client = new RestClient("https://petstore.swagger.io/v2");
+            RestRequest request = new RestRequest("pet/813", Method.Delete);
+            request.AddHeader("api_key", "jsdjsdjdsj");
+
+
+            var response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+
+        }
+    }
+}
